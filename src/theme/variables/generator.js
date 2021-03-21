8cb4406276617d10298408b7ccb820a5d1cd7741
psoaresbj <psoares.bj@gmail.com>
2021-03-21 00:46:04 +0000

    update generator

--- src/theme/variables/generator.js
+++ src/theme/variables/generator.js
@@ -7,7 +7,7 @@ import { alignments } from './alignments';
 import { colors } from './colors';
 import { flexFunctions } from '../helpers/flex';
 import { fonts } from './fonts';
-import { margin, padding, size } from 'polished';
+import { margin, padding, position, size } from 'polished';
 
 export const generator = {
   // Named props
@@ -15,6 +15,7 @@ export const generator = {
     { prefix: 'bg', list: colors, cssProp: 'background-color' },
     { list: colors, cssProp: 'color' },
     { list: fonts.weights, cssProp: 'font-weight' },
+    { prefix: 'text', list: alignments, cssProp: 'text-align' },
     { prefix: 'flex', list: { wrap: 'wrap', nowrap: 'nowrap' }, cssProp: 'flex-wrap' }
   ],
 
@@ -30,6 +31,7 @@ export const generator = {
     // fn props
     { name: 'm', helperFn: margin, units: 'rem' },
     { name: 'p', helperFn: padding, units: 'rem' },
+    { name: 'pos', helperFn: position, units: 'rem' },
     { name: 'size', helperFn: size, units: 'rem' },
     ...Object.keys(flexFunctions).map(name => ({ name, helperFn: flexFunctions[name] }))
   ]
